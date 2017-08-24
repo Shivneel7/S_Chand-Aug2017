72d51c28850cef0d007bb1d3cd6c2f8724bfaafe
18chando40 <18chando40@millshighsch.org>
2017-08-23 20:13:59 -0700

    1

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,10 @@
+//@author Shivneel Chand
+//@version 1.3
+public class Hello_world {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		//displays the string 
+	}
+
+}
