d8babb4cfe3571edfa60fda03ccdfb63a5b54d92
18chando40 <18chando40@millshighsch.org>
2017-08-24 09:30:42 -0700

    7th

---
+++ Project1/src/Project1.java
@@ -0,0 +1,12 @@
+/* This class comapres output using print and println
+
+ */
+public class Project1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World.");
+		System.out.print("Aga");
+		System.out.print("in.");
+	}
+
+}
