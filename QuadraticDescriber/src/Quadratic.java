1167b7bc612c06a8aaabe243f508bc710524278b
18chando40 <18chando40@millshighsch.org>
2017-09-25 09:41:10 -0700

    Started part 5

---
+++ QuadraticDescriber/src/Quadratic.java
@@ -0,0 +1,8 @@
+//Shivneel Chand
+//9-25-17
+//This describes the quadratic graph to the user.
+public class Quadratic {
+	public static void quadrDescriber(double a, double b, double c) {
+		
+	}
+}
---
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -0,0 +1,11 @@
+//Shivneel Chand
+//9-25-17
+//This program will receive a quadratic formula from the user and will send the values to Quadratic class.
+import java.util.*;
+public class QuadraticClient {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
