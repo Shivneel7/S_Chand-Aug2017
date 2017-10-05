b3833d1ec3c9201d48ee216bd31059f53a193b55
18chando40 <18chando40@millshighsch.org>
2017-10-04 19:52:58 -0700

    Finished pair programming

--- Pair Programming/src/ProcessingNumbers.java
+++ Pair Programming/src/ProcessingNumbers.java
@@ -1,17 +1,17 @@
 //Programmer 1 Shivneel Chand and Programmer 2 James Lee partner code.
-
 import java.util.*;
 public class ProcessingNumbers {
 
 	public static void main(String[] args) {
 		Scanner userInput = new Scanner(System.in);
-		System.out.println("How many terms are you going to write?");
+		System.out.println("How many terms are you going to list?");
 		double numberOfTerms = userInput.nextDouble();
-		double maxEven = -1;
+		double maxEven = -1;//Later, I test if maxEven == -1 and print "No even numbers" if true.
 		double max = 0;
 		double min = 0;
 		double evenSum = 0;
 		boolean isFirstNumber = true;
+		boolean isFirstEvenNumber = true;
 		for(int i = 1; i <= numberOfTerms; i++) {
 			System.out.println("Enter number " + i + ": ");
 			double num = userInput.nextDouble();
@@ -19,28 +19,29 @@ public class ProcessingNumbers {
 				min = num;
 				max= num;
 				isFirstNumber = false;
-			}else {
-				if(num > max) {
-					max = num;
+			}
+			if(num > max) {
+				max = num;
+			}
+			if(num < min) {
+				min = num;
+			}
+			if(num %2 == 0) {
+				if(isFirstEvenNumber) {//only executes for the first even number
+					maxEven = num;
+					isFirstEvenNumber= false;
+				}else if(num > maxEven) {
+					maxEven = num;
 				}
-				if(num < min) {
-					min = num;
-				}
-				if(num %2 == 0) {
-					if(num > maxEven) {
-						maxEven = num;
-					}
-				evenSum += num;
-				}
+			evenSum += num;
 			}
 		}
 		System.out.println("The lowest number is: " + min + " The greatest number is: " + max);
-		System.out.println("The sum of even is: " + evenSum);
 		if(maxEven == -1) {
-			System.out.println("Your list only had odd numbers.");
+			System.out.println("Your list contained no even numbers.");
 		}else {
+			System.out.println("The sum of the even numbers is: " + evenSum);
 			System.out.println("The largest even number is: " + maxEven);
 		}
-		
 	}
 }
