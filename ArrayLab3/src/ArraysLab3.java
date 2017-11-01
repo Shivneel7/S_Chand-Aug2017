0b714c59d9bec89a987eaa9e0cd3940b4cd48792
18chando40 <18chando40@millshighsch.org>
2017-11-01 10:48:32 -0700

    Started array lab 3

---
+++ ArrayLab3/src/ArraysLab3.java
@@ -0,0 +1,21 @@
+//Shivneel Chand
+//11-1-17
+//Arrays Lab 3
+public class ArraysLab3 {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static int[] sum(int[] arr1, int[] arr2) {
+		int[] sum = new int[arr1.length];
+		for(int i = 0; i < arr1.length;i++) {
+			sum[i] = arr1[i]+arr2[i];
+		}
+		return sum;
+	}
+	
+	public static int
+
+
+}
