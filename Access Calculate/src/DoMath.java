cd2a681ab91ccb7349481ed968a0c79d52cb8d74
18chando40 <18chando40@millshighsch.org>
2017-11-15 23:05:35 -0800

    consistency is key

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -12,15 +12,15 @@ public class DoMath {
 //		System.out.println(Calculate.toRadians(90));
 //		System.out.println(Calculate.toRadians(90));
 //		System.out.println(Calculate.discriminant(7, 2, -3));
-//		System.out.println(Calculate.toImproperFrac(4,7,8));
-//		System.out.println(Calculate.toMixedNum(39,8));
+		System.out.println(Calculate.toImproperFrac(-4,7,8));
+		System.out.println(Calculate.toMixedNum(-39,8));
 //		System.out.println(Calculate.foil(2,3,6,-7, "n"));
 //		System.out.println(Calculate.isDivisibleBy(4,-2));
 //		System.out.println(Calculate.absValue(-7));
 //		System.out.println(Calculate.max(11,7, 11));
 //		System.out.println(Calculate.min(-14,-12));
 //		System.out.println(Calculate.round2(-1031.6312));
-		System.out.println(Calculate.exponent(2, 5));
+//  	System.out.println(Calculate.exponent(2, 5));
 //		System.out.println(Calculate.factorial(0));
 //		System.out.println(Calculate.isPrime(4));
 //		System.out.println(Calculate.gcf(48,18));
