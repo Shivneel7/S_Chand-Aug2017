287bcc08094e9650a2fee3b96d4bc5ab975616e1
18chando40 <18chando40@millshighsch.org>
2017-11-16 19:28:36 -0800

    Finished part 3

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -32,21 +32,23 @@ public class FracCalc {
         int[] operand1 = splitOperand(parsedInput[0]);
         int[] operand2 = splitOperand(parsedInput[2]);
         
+        String result = "";     
         String operator = parsedInput[1];
-        String result = "";
+        
         if(operator.equals("+") || operator.equals("-")) {
         	result = addSubtract(operand1, operand2, operator);
         }else if(operator.equals("*") || operator.equals("/")) {
         	result = multiplyDivide(operand1, operand2, operator);
-        }else {
-        	//MOVE THIS TO THE MULTIPLYDIVIDE METHOD
-
         }
+        
+        //result = simplify(splitOperand(result));
+        
         return result;
     }
     
+    //Takes an operand and returns the whole number, numerator, and denominator of the operand in an array of length 3.
     public static int[] splitOperand(String operand) {
-        int numerator = 0;
+    	int numerator = 0;
         int denominator = 1;
    	 	int wholeNumber = 0;
         if(operand.contains("/") && operand.contains("_")) {
@@ -62,29 +64,35 @@ public class FracCalc {
         if(wholeNumber<0) {
         	numerator *= -1;
         }
+        //numerator = wholeNumber * denominator + numerator;
         int[] operandParts = {wholeNumber, numerator, denominator};
         return operandParts;
     }
-    
+    //2_1/2 - 5/2
     public static String addSubtract(int[] operand1, int[] operand2, String operator) {
     	int newDenominator = operand1[2] * operand2[2];
     	int numerator1 = operand1[1] * operand2[2];
     	int numerator2 = operand2[1] * operand1[2];
     	int newNumerator = numerator1 + numerator2;
     	int newWholeNumber = operand1[0] + operand2[0];
+
     	if(operator.equals("-")) {
     		newNumerator = numerator1 - numerator2;
     		newWholeNumber = operand1[0] - operand2[0];
     	}
+    	if(newWholeNumber != 0 && newNumerator < 0) {
+    		newNumerator *= -1;
+    	}
+    	//System.out.println("newWholeNumber " + newWholeNumber + " newNumerator " + newNumerator);
     	return newWholeNumber + "_" + newNumerator + "/" + newDenominator;
     }
-    
+
     public static String multiplyDivide(int[] operand1, int[] operand2, String operator) {
     	int numerator1 = operand1[0] * operand1[2] + operand1[1];
     	int numerator2 = operand2[0] * operand2[2] + operand2[1];
-	    if(operator.equals("/")){
-    		int temp = operand2[1];
-	    	operand2[1] = operand2[2];
+	    if(operator.equals("/")) {
+    		int temp = numerator2;
+	    	numerator2 = operand2[2];
 	    	operand2[2] = temp;
 	    }
     	int newNumerator = numerator1 * numerator2;
@@ -93,5 +101,22 @@ public class FracCalc {
     	}
     	int newDenominator = operand1[2] * operand2[2];
     	return newNumerator + "/" + newDenominator;
+    }
+    
+    public static String simplify(int[] frac) {
+    	int numerator = frac[0] * frac[2] + frac[1];
+    	int denominator = frac[2];
+		System.out.println(numerator);
+    	if(numerator == 0) {
+			return "0";
+		}
+    	
+    	int wholeNumber = numerator/denominator;
+		numerator = numerator%denominator;
+		if(wholeNumber == 0) {
+			return numerator + "/" + denominator;
+		}
+		return wholeNumber + "_" + numerator + "/" + denominator;
+
     }
 }
