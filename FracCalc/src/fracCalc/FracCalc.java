108398d795e9645e2dac0d40db6c1b45ee6bde92
18chando40 <18chando40@millshighsch.org>
2017-11-20 21:47:43 -0800

    Finished Extra Credit

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -28,37 +28,44 @@ public class FracCalc {
     // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
     //      e.g. input ==> "1/2 + 3/4"
     //        
-    // The function should return the result of the fraction after it has been calculated
+    // The method returns the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
     public static String produceAnswer(String input){
     	String[] parsedInput = input.split(" ");
-        if(parsedInput.length != 3) {
-        	return "Your input is not acceptable, please try again.";
-        }
-        int[] operand1 = splitOperand(parsedInput[0]);
-        int[] operand2 = splitOperand(parsedInput[2]);
-        
-        String result = "";     
-        String operator = parsedInput[1];
-        
-        if(operator.equals("+") || operator.equals("-")) {
-        	result = addSubtract(operand1, operand2, operator);
-        }else if(operator.equals("*")) {
-        	result = multiplyDivide(operand1, operand2, operator);
-        }else if(operator.equals("/")){
-        	if(operand2[0] == 0 || operand2[1] == 0) {//Makes sure we do not divide by 0
-        		return "Cannot divide by zero";
-        	}
-        	result = multiplyDivide(operand1, operand2, operator);
-        }else {
-        	return "I can't do that. Please try a different operator.";
-        }
+    	String resultString = "";
+    	for(int i = 2; i < parsedInput.length; i += 2) {
+	        int[] operand1 = splitOperand(parsedInput[0]);
+	        int[] operand2 = splitOperand(parsedInput[i]);
 
-        result = simplify(splitOperand(result));
+	        String operator = parsedInput[i-1];
+	        
+	        int[] unsimplifiedResultArray = new int[2];
+	        if(operand1[1] == 0 || operand2[1] == 1) {//checks is denominator of any operands is 0, so we don't try and divide by 0.
+	        	return "Please stop trying to divide by 0.";
+	        }
+	        if(operator.equals("+") || operator.equals("-")) {
+	        	unsimplifiedResultArray = addSubtract(operand1, operand2, operator);
+	        
+	        }else if(operator.equals("*")) {
+	        	unsimplifiedResultArray = multiplyDivide(operand1, operand2, operator);
+	        
+	        }else if(operator.equals("/")){
+	        	if(operand2[0] == 0) {//Makes sure we do not divide by 0.
+	        		return "Please stop trying to divide by 0.";
+	        	}
+	        	unsimplifiedResultArray = multiplyDivide(operand1, operand2, operator);
+	        }else {
+	        	return "I can't do that. Please try a different operator.";
+	        }
 
-        return result;
+	        resultString = simplify(unsimplifiedResultArray);
+	        parsedInput[0] = resultString;	//by saving this in the 0th index of the original array,
+	        								//we can always make parsedInput[0] the first operand 
+	        								//and easily handle multiple operands
+    	}
+        return resultString;
     }
-    
+
     //Takes an operand and returns the whole number, numerator, and denominator of the operand in an array of length 3.
     public static int[] splitOperand(String operand) {
     	int numerator = 0;
@@ -81,40 +88,41 @@ public class FracCalc {
         int[] operandParts = {numerator, denominator};
         return operandParts;
     }
+    
 //This method returns the sum or the difference of the two operands depending on the operator passed to the method
-    public static String addSubtract(int[] operand1, int[] operand2, String operator) {
+    public static int[] addSubtract(int[] operand1, int[] operand2, String operator) {
     	int newDenominator = operand1[1] * operand2[1];
     	int numerator1 = operand1[0] * operand2[1];
     	int numerator2 = operand2[0] * operand1[1];
     	//Addition
     	int newNumerator = numerator1 + numerator2;
-
-    	if(operator.equals("-")) { // subtraction
+    	//Subtraction
+    	if(operator.equals("-")) {
     		newNumerator = numerator1 - numerator2;
     	}
-
-    	return newNumerator + "/" + newDenominator;
+    	int[] result = {newNumerator, newDenominator};
+    	return result;
     }
-//this method returns the product or quotient of the two operands depending on the operator passed to the method
-    public static String multiplyDivide(int[] operand1, int[] operand2, String operator) {
-	    if(operator.equals("/")) {
-	    	if(operand2[0] < 0) {//this is too make sure any negative signs stay with the numerator
-    			operand2[1] *= -1;
+    
+//returns the product or quotient of the two operands depending on the operator passed to the method
+    public static int[] multiplyDivide(int[] operand1, int[] operand2, String operator) {
+    	if(operator.equals("/")) {//Division part
+	    	if(operand2[0] < 0) {	//<--- this is to make sure any negative signs stay with
+    			operand2[1] *= -1;	//the numerator which helps the simplification process
     			operand2[0] *= -1;
     		}
-	    	int temp = operand2[0]; //reciprocates the second operand
+	    	int temp = operand2[0]; //Reciprocates the second operand
 	    	operand2[0] = operand2[1];
 	    	operand2[1] = temp;
 	    }
+	    //Multiplication
     	int newNumerator = operand1[0] * operand2[0];
-    	if(operand2[1] == 0) {
-    		return newNumerator + "/" + operand2[0];
-    	}
     	int newDenominator = operand1[1] * operand2[1];
-    	return newNumerator + "/" + newDenominator;
+    	int[] result = {newNumerator, newDenominator};
+    	return result;
     }
     
-//Simplifies the result.
+//Simplifies the result. Takes an array with the numerator and denominator at index 0 and 1 respectively.
     public static String simplify(int[] frac) {
     	int wholeNumber = frac[0]/frac[1];
     	int numerator = frac[0] % frac[1];
@@ -122,7 +130,7 @@ public class FracCalc {
     	if(wholeNumber != 0) { // if the number is a fraction with no whole number, then make the numerator...
     		numerator = Math.abs(numerator);//positive since the whole number contains the negative sign.
     	}
-		//find the gcf then divide numerator and denominator by it to reduce the fraction to its simplest form.
+		//finds the gcf then divide numerator and denominator by it to reduce the fraction to its simplest form.
     	int gcf = findGCF(numerator, denominator);
     	numerator /= gcf;
 		denominator /= gcf;
@@ -137,6 +145,7 @@ public class FracCalc {
 		}
 		return wholeNumber + "_" + numerator + "/" + denominator;
     }
+    
 //takes two integers and returns the greatest common factor of the integers.
     public static int findGCF(int num1, int num2) {
     	num1 = Math.abs(num1);
