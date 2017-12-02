4e24d1ca6e9c1c6e94316cd53587dbea0c0739a8
18chando40 <18chando40@millshighsch.org>
2017-12-02 02:21:27 -0800

    idek

--- Game/src/game/MouseInput.java
+++ Game/src/game/MouseInput.java
@@ -14,7 +14,7 @@ public class MouseInput extends MouseInputAdapter{
 	}
 
 	public void mouseMoved(MouseEvent e){
-		//System.out.println(e.getX() + " " + e.getY());
+		System.out.println(e.getX() + " " + e.getY());
 		
 		for(int i = 0; i < handler.object.size(); i++) {
 			if(handler.object.get(i).getID() == ID.Player) {
