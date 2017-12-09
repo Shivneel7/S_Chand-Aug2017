43142e155a74b16f56193c9b691d1ed2d7433a1d
18chando40 <18chando40@millshighsch.org>
2017-12-08 22:13:56 -0800

    Removed fixed numbers

--- FlappyBird/src/derpyBurd/Pipe.java
+++ FlappyBird/src/derpyBurd/Pipe.java
@@ -10,13 +10,14 @@ public class Pipe {
 	private int x, y;
 	private Background b;
 	private int pipeWidth = 40;
+	private int gap = 90;
 	
 	Random r = new Random();
 	
 	public Pipe(Background b) {
 		this.b = b;
 		this.x = b.getX() - 60;
-		this.y = r.nextInt(195) + 200;
+		this.y = r.nextInt(Game.HEIGHT/2 - 5) + Game.HEIGHT/2;
 	}
 	
 	public int getX() {
@@ -36,13 +37,13 @@ public class Pipe {
 	}
 	
 	public Rectangle getBounds2() {
-		return new Rectangle(x, Game.HEIGHT - y + 90, pipeWidth, y - 90);
+		return new Rectangle(x, Game.HEIGHT - y + gap, pipeWidth, y - gap);
 	}
 	
 	public void render(Graphics g) {
 		g.setColor(Color.green);
 		g.fillRect(x, 0, pipeWidth, Game.HEIGHT - y);
-		g.fillRect(x, Game.HEIGHT - y + 90, pipeWidth, y - 90);
+		g.fillRect(x, Game.HEIGHT - y + gap, pipeWidth, y - gap);
 	}
 	
 	public void getRandomY() {
@@ -52,7 +53,7 @@ public class Pipe {
 	public void tick() {
 		this.x = b.getX() - pipeWidth;
 		if(x == (Game.WIDTH % Game.SPEED) - pipeWidth) {
-			y = r.nextInt(195) + 200;
+			y = r.nextInt(Game.HEIGHT/2 - 5) + Game.HEIGHT/2;
 		}
 	}
 	
