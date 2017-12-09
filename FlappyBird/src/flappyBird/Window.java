e5b0dab8ea28d1a13b29ebcc91c718619865e032
18chando40 <18chando40@millshighsch.org>
2017-12-08 19:40:41 -0800

    Made window nicer

--- FlappyBird/src/flappyBird/Window.java
+++ FlappyBird/src/flappyBird/Window.java
@@ -7,15 +7,15 @@ import javax.swing.JFrame;
 public class Window extends Canvas{
 	public Window(int width, int height, String title, Game game) {
 		JFrame frame = new JFrame(title);
-		
+		frame.add(game);
+		game.requestFocus();
 		frame.setSize(width, height);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 		frame.setVisible(true);
-		frame.add(game);
+		
 		game.start();
-		game.requestFocus();
 	}
 	
 	
