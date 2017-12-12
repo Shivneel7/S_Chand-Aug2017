0d82566a16d09918d9482a7c6498078b3b94022e
18chando40 <18chando40@millshighsch.org>
2017-12-11 20:39:18 -0800

    test

--- Game/src/game/Player.java
+++ Game/src/game/Player.java
@@ -8,8 +8,14 @@ public class Player extends GameObject{
 
 	Random r = new Random();
 	
+	private BufferedImageLoader loader;
+	
+	private SpriteSheet ss;
+	
 	public Player(int x, int y, ID id) {
 		super(x, y, id);
+		loader = new BufferedImageLoader();
+		 ss = new SpriteSheet(loader.loadImage("/Untitled.png"));
 	}
 
 	public void tick() {
@@ -19,11 +25,12 @@ public class Player extends GameObject{
 	}
 
 	public void render(Graphics g) {
-		g.setColor(Color.blue);
-		g.fillRect(x, y, 32, 32);
-		g.setColor(Color.white);
-		g.fillRect(x+6, y+6, 4, 4);
-		g.fillRect(x+22, y+6, 4, 4);
+//		g.setColor(Color.blue);
+//		g.fillRect(x, y, 32, 32);
+//		g.setColor(Color.white);
+//		g.fillRect(x+6, y+6, 4, 4);
+//		g.fillRect(x+22, y+6, 4, 4);
+		g.drawImage(ss.grabImage(1, 1, 32, 32), x, y, null);
 	}
 	
 
