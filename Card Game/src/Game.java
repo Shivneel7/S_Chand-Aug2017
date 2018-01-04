475eb3443cb5973e7a2e213768de1d7c00f0ffd9
18chando40 <18chando40@millshighsch.org>
2018-01-04 14:28:16 -0800

    a

---
+++ Card Game/src/Game.java
@@ -0,0 +1,92 @@
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+public class Game extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = -8921419424614180143L;
+	
+	public final int GAME_WIDTH = 800, GAME_HEIGHT = GAME_WIDTH/12 * 9;
+
+	private Thread thread;
+	private boolean running = false;
+
+	public Game() {
+		new Window("Cards", GAME_WIDTH, GAME_HEIGHT, this);
+		
+	}
+	
+	public static void main(String[] args) {
+		new Game();
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics g = bs.getDrawGraphics();
+		/////////////////////////////////////////////////////
+		
+		g.setColor(Color.green);
+		g.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+		
+		/////////////////////////////////////////////////////
+		g.dispose();
+		bs.show();
+	}
+
+	public void run() {
+		requestFocus();
+		long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while(running) {
+        	long now = System.nanoTime();
+        	delta += (now - lastTime) / ns;
+        	lastTime = now;
+        	while(delta >=1) {
+        		tick();
+        		delta--;
+        	}
+        	if(running) {
+        		render();
+        	}
+        	frames++;
+        	if(System.currentTimeMillis() - timer > 1000){
+				timer += 1000;
+				System.out.println("FPS: "+ frames);
+				frames = 0;
+        	}
+        }
+        stop();
+	}
+
+	public synchronized void start() {
+		if(!running) {
+			thread = new Thread(this);
+			thread.start();
+			running = true;
+		}else
+			return;
+	}
+	
+	public synchronized void stop() {
+		try {
+			thread.join();
+			running = false;
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ Card Game/src/Window.java
@@ -0,0 +1,22 @@
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Window extends Canvas {
+
+	private static final long serialVersionUID = -8255319694373975038L;
+
+	public Window(String t, int w, int h, Game game) {
+		JFrame frame = new JFrame(t);
+		frame.setResizable(false);
+		game.setPreferredSize(new Dimension(w,h));
+		frame.add(game);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		//frame.setLocation(1450, 100); //for my second monitor at home.
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		game.start();
+	}
+}
