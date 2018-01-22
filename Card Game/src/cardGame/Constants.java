feec7c608ed66f08431ccbad97d0c506b5855db4
18chando40 <18chando40@millshighsch.org>
2018-01-22 09:49:19 -0800

    c

--- Card Game/src/cardGame/Constants.java
+++ Card Game/src/cardGame/Constants.java
@@ -9,8 +9,8 @@ public interface Constants {
 	public static final int SPACE = 20;
 	public static final int STACK_SPACING = 20;
 
-	public static final int CARD_WIDTH = (GAME_WIDTH-((SLOTS + 2) * SPACE))/SLOTS;
-	public static final int CARD_HEIGHT = (int) (CARD_WIDTH * 1.4);
+	public static final int CARD_WIDTH = (GAME_WIDTH-((SLOTS + 2) * SPACE))/SLOTS; //88
+	public static final int CARD_HEIGHT = (int) (CARD_WIDTH * 1.4); //123
 
 	public static final int WASTEPILE_SPACING = CARD_WIDTH/4 * 3;
 	
