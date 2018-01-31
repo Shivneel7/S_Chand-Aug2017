3d5bfe88473416858041c9e1b588568b017f47a5
18chando40 <18chando40@millshighsch.org>
2018-01-31 10:47:24 -0800

    a

---
+++ ArrayLab3/src/TestChild.java
@@ -0,0 +1,8 @@
+
+public class TestChild extends TestSuper {
+	public TestChild() {
+		this.a = 9;
+		this.b = 10;
+		this.c = 12;
+	}
+}
---
+++ ArrayLab3/src/TestSuper.java
@@ -0,0 +1,12 @@
+
+public class TestSuper {
+	private int a;
+	public int b;
+	protected int c;
+	
+	public TestSuper(){
+		a = 0;
+		b = 1;
+		c = 7;
+	}
+}
