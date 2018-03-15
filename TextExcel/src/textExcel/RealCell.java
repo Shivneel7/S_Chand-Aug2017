b65dd0ffb574063a62057c4b55c38af79dc4b233
18chando40 <18chando40@millshighsch.org>
2018-03-15 09:49:33 -0700

    asdf

---
+++ TextExcel/src/textExcel/RealCell.java
@@ -0,0 +1,11 @@
+package textExcel;
+
+public abstract class RealCell implements Cell {
+
+	public abstract String abbreviatedCellText();
+
+	public abstract String fullCellText();
+	
+	public abstract double getDoubleValue();
+
+}
