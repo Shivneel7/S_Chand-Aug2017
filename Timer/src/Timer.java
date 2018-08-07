24b871e7b43abfca5edab1e5ed337ecdf282b2fb
shivn <shivn@LAPTOP-HO3SN0IB.attlocal.net>
2018-08-07 00:18:25 -0700

    a

---
+++ Timer/src/Timer.java
@@ -0,0 +1,17 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class Timer {
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		JPanel p = new JPanel();
+		p.setSize(250, 250);
+		frame.add(p);
+		frame.setSize(250,250);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+}
