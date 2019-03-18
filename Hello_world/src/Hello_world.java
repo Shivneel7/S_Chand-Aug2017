1f6d89db4b69d334e359668797393a0c2c5df6ea
shivn <shivn@10.34.95.41>
2019-03-18 16:18:45 -0700

    n

--- Hello_world/src/Hello_world.java
+++ Hello_world/src/Hello_world.java
@@ -2,9 +2,9 @@
 //@version 1.3
 public class Hello_world {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		//displays the string 
-	}
+	public static void main(String[] args) {	
+		
+		System.out.println("Hello World!");	
 
+	}
 }
